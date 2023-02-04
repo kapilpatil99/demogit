dd34313b119b704f239ae299d5ed60867f8cea5d
kapil <patilkapil49@gmail.com>
2023-02-04 10:40:03 +0530

    update new code for run

--- login.java
+++ login.java
@@ -1,3 +1,37 @@
+package sunbeam;
+
+class binarysearchtree{
+	static class node{
+		//node field
+		private int data;
+		private node left,right;
+	     //node method
+		public node() {
+			data=0;
+			left  = null;
+			right = null;
+		    }
+		public node(int val) {
+			data=val;
+			left  = null;
+			right = null;
+		    }
+		}
+	
+    // tree fields
+	private node root;
+
+	//tree method
+	public binarysearchtree() {
+		root = null;
+		
+		}
+	void add(int val) {
+		node newnode = new node(val);
+		
+	}
+	}
+	
 
 public class BSTtreemain {
 	public static void main(String[] args) {
