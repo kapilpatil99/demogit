2e70e78c4acb12ef11b4bf1b23d8f8396bb21e51
kapil <patilkapil49@gmail.com>
2023-02-04 10:35:37 +0530

    this is java file

---
+++ login.java
@@ -0,0 +1,18 @@
+
+public class BSTtreemain {
+	public static void main(String[] args) {
+		binarysearchtree t= new binarysearchtree();
+		t.add(50);
+		t.add(30);
+		t.add(10);
+		t.add(90);
+		t.add(100);
+		t.add(40);
+		t.add(70);
+		t.add(80);
+		t.add(60);
+		t.add(20);
+		
+		
+	}
+}
